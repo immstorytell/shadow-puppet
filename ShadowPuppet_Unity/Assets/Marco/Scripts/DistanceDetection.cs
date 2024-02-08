e5b7857c2da8367deb9c01965112f1bded3fcbd2
MarcoLuna31 <157730968+MarcoLuna31@users.noreply.github.com>
2024-02-08 13:29:57 -0500

    added distance detection script

---
+++ ShadowPuppet_Unity/Assets/Marco/Scripts/DistanceDetection.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceDetection : MonoBehaviour
+{
+    public float Distance;
+    public Transform A;
+    public Transform B;
+
+    public float DistanceX;
+    public float DistanceY;
+    public float DistanceZ;
+
+    // Start is called before the first frame update
+    
+    void Start () {
+
+      
+   
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 positionA = A.position;
+        Vector3 positionB = B.position;
+
+        Distance = Vector3.Distance(positionA,positionB);
+
+        DistanceX = positionA.x - positionB.x;
+        DistanceY = positionA.y - positionB.y;
+        DistanceZ = positionA.z - positionB.z;
+
+    }
+}
